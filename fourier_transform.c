633934b2d0d837f642911d203c0b46a5a0fa673e
A2TY <a2ty.dev@gmail.com>
2015-10-11 17:21:15 +0900

    小数の入力値に対応，表示レイアウトの変更

--- fourier_transform.c
+++ fourier_transform.c
@@ -3,7 +3,7 @@
 
 int main() {
 
-    int sigX[100] = {0};    // 入力信号
+    double sigX[100] = {0};    // 入力信号
     int smp = 0;            // 入力信号のサンプリング数
     int mul = 0;            // 乗数
     int tmp1;               // アダマール行列作成時の途中計算（行列数の決定）
@@ -13,17 +13,17 @@ int main() {
     printf("信号を入力（10000以上を入力して入力終了）\n");    // フーリエ変換する信号を入力
     for (int i = 0; sigX[smp] < 10000; i++, smp = i - 1) {
         printf("> ");
-        scanf("%d", &sigX[i]);
+        scanf("%lf", &sigX[i]);
     }
     sigX[smp] = 0;  // 最後に入力された10000以上の値を削除
 
 
     for (int i = 0; tmp1 < smp; i++) {  // アダマール行列の行数・列数を決定（n = 2^m）
         tmp1 = (int)pow(2, i);
-        printf("i = %d\n", i);
+        // printf("i = %d\n", i);
     }
     smp = tmp1;
-    printf("smp = %d\n", smp);    // smpの値確認
+    // printf("smp = %d\n", smp);    // smpの値確認
 
 
     int sigY[smp][smp]; // アダマール行列を代入する配列
@@ -34,13 +34,14 @@ int main() {
         for (int j = 0; j < smp; j++) {
             mul = 0;
             tmp2 = i & j;
-            printf("%d %d , tmp2 = %d\n", i, j, tmp2);   // 乗数の計算過程
+            // printf("%d %d , tmp2 = %d\n", i, j, tmp2);   // 乗数の計算過程
             for (int k = 0; tmp2 > 0; k++) {
                 mul += tmp2 % 2;
                 tmp2 /= 2;
             }
-            printf("mul = %d\n", mul);  // 乗数の計算結果
+            // printf("mul = %d\n", mul);  // 乗数の計算結果
             sigY[i][j] = (int)pow(-1, mul);
+            //printf("sigY[%d][%d] = %d\n", i, j, sigY[i][j]);
             }
     }
 
@@ -56,21 +57,21 @@ int main() {
 
     printf("\n==入力信号==\n");
     for (int i = 0; i < smp; i++) {
-        printf("%d\n", sigX[i]);
+        printf("%9f\n", sigX[i]);
     }
 
     printf("\n==アダマール行列==\n");
     for (int i = 0; i < smp; i++) {
-        printf("信号%d = ", i + 1);
+        printf("信号%2d = ", i + 1);
         for (int j = 0; j < smp; j++) {
-            printf("%d\t", sigY[i][j]);
+            printf("%2d\t", sigY[i][j]);
         }
         printf("\n");
     }
 
     printf("\n==相関値==\n");
     for (int i = 0; i < smp; i++) {
-        printf("%f\n", cor[i]);
+        printf("%9f\n", cor[i]);
     }
 
     return 0;
